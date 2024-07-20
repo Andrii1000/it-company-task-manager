a52569c4291e1b0a0d43f9874efab3414e397e42
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-07-20 10:21:49 +0300

    add model to admin panel & customizing it

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from catalog.models import (
+    TaskType,
+    Position,
+    Worker,
+    Task
+)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "priority", "deadline", "task_type", "is_completed")
+    search_fields = ("name",)
+    list_filter = ("is_completed", "task_type", "priority",)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("position",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(TaskType)
+admin.site.register(Position)
