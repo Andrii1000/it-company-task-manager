902f597694e17f52e3460a0f7461e0a2b03c77b5
Andrii1000 <127493083+Andrii1000@users.noreply.github.com>
2024-07-21 13:51:09 +0300

    add all urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import index, TaskListView, WorkerListView, WorkerDetailView, WorkerCreateView, WorkerUpdateView, \
+    WorkerDeleteView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>", TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>", WorkerDetailView.as_view(), name="worker-detail"),
+    path("workers/create", WorkerCreateView.as_view(), name="worker-create"),
+    path("workers/<int:pk>/update", WorkerUpdateView.as_view(), name="worker-update"),
+    path("workers/<int:pk>/delete", WorkerDeleteView.as_view(), name="worker-delete"),
 ]
 
 app_name = "catalog"
